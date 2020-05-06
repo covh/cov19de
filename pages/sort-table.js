e695f89c9885fc5343a34fc470f2fe43a66adb32
Andreas Krueger <covid19hacks@protonmail.com>
2020-05-06 05:26:47 +0200

    files-batch-updated-at_20200506-0526

--- pages/sort-table.js
+++ pages/sort-table.js
@@ -26,6 +26,8 @@ function sortTable(tid, n, cid) {
 	
 function sortTable_afterDelay(tid, n, cid) {
 	// 
+	// default (first) direction is now DESCENDING, second click then ASCENDING 
+	//
 	// https://www.w3schools.com/howto/howto_js_sort_table.asp
 	// 
 	// <table id="myTable2">
@@ -38,24 +40,22 @@ function sortTable_afterDelay(tid, n, cid) {
   table = document.getElementById(tid);
   // console.log(table);
   switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
+  // Set the sorting direction to descending:
+  dir = "desc";
 
-  /* Make a loop that will continue until
-  no switching has been done: */
+  /* Make a loop that will continue until no switching has been done: */
   while (switching) {
     // Start by saying: no switchingremove  is done:
     switching = false;
     rows = table.rows;
 	// console.log(rows);
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
+	
+    /* Loop through all table rows (except the first, which contains table headers): */
     for (i = 1; i < (rows.length - 1); i++) {
       // Start by saying there should be no switching:
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
+      /* Get the two elements you want to compare, one from current row and one from the next: */
+      x = rows[i]    .getElementsByTagName("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
 
 	  // generate the entities that can be compared:
@@ -70,17 +70,14 @@ function sortTable_afterDelay(tid, n, cid) {
 	  }
 	  // console.log(yy);
 
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
+      /* Check if the two rows should switch place, based on the direction, asc or desc: */
       if (dir == "asc") {
-        // if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) { // alphabetically
 		if (xx > yy) {
           // If so, mark as a switch and break the loop:
           shouldSwitch = true;
           break;
         }
       } else if (dir == "desc") {
-        // if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) { // alphabetically
 		if (xx < yy) {
           // If so, mark as a switch and break the loop:
           shouldSwitch = true;
@@ -89,17 +86,15 @@ function sortTable_afterDelay(tid, n, cid) {
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
+      /* If a switch has been marked, make the switch and mark that a switch has been done: */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
       // Each time a switch is done, increase this count by 1:
       switchcount ++;
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
+      /* If no switching has been done AND the direction is "desc", set the direction to "asc" and run the while loop again. */
+	  if (switchcount == 0 && dir == "desc") {
+		dir = "asc";
         switching = true;
       }
     }
